142fca44b8c4010d3a0330b173830cd30278c9e7
evelan <evelan3094@gmail.com>
2015-05-31 15:59:43 +0200

    zadania2

--- main.asm
+++ main.asm
@@ -14,8 +14,7 @@ DANE SEGMENT USE16
 ;Tablica deskryptorow przerwan IDT
 	IDT	LABEL WORD
 	INCLUDE   PM_IDT.TXT
-  IDT_0	    INTR <PROC_0>	          ;Przerwanie zegarowe (trap zadania)
-  IDT_1	    INTR <PROC_1>	      
+  IDT_0	    INTR <PROC_0>	          ;Przerwanie zegarowe (trap zadania)    
 	IDT_SIZE = $ - IDT
 	PDESKR	  DQ 	0
 	ORG_IDT	  DQ	0
@@ -34,6 +33,8 @@ DANE SEGMENT USE16
 	TASK1_OFFS DW 0
 	TASK1_SEL  DW 48
 	
+	T1_ADDR   DW 0,40
+	
 	INFO_INR DB 'przerwanie'
 	INFO_Z_1 DB '1'
 	INFO_Z_2 DB '2'
@@ -43,7 +44,7 @@ DANE SEGMENT USE16
 	POZYCJA_2       DW 800;2560
 	POZYCJA         DW 0	
 	
-	AKTYWNE_ZADANIE DW 1
+	AKTYWNE_ZADANIE DW 0
 	
 	TIME	    DB    0
 	KOLOR	    DB    71H
@@ -64,32 +65,26 @@ PROC_0	PROC
 	PUSH AX
 	PUSH BX
 
-  CMP AKTYWNE_ZADANIE,0
-  JE  TSK0
+  ;CMP AKTYWNE_ZADANIE,0
+  ;JE  TSK0
 
-  CMP AKTYWNE_ZADANIE,1
-  JE  TSK1
-  
-	MOV AL,20H
-	OUT 20H,AL
+  ;CMP AKTYWNE_ZADANIE,1
+  ;JE  TSK1
 	
-	
-	TSK0:	
-  CALL DWORD PTR TASK0_OFFS	;PRZE£¥CZENIE ZADANIA
-  ;MOV AKTYWNE_ZADANIE, 
+	TSK0:
+	;JMP DWORD PTR T1_ADDR	
+
+  ;CALL DWORD PTR TASK0_OFFS	;PRZE£¥CZENIE ZADANIA
+  ;MOV AKTYWNE_ZADANIE, 1
 	
 	TSK1:
   CALL DWORD PTR TASK1_OFFS
   ;MOV AKTYWNE_ZADANIE, 0
-	
+  
 	POP BX
 	POP AX
 	IRETD
 PROC_0	ENDP
-
-PROC_1	PROC
-	
-PROC_1	ENDP
 
 START:	
 	INICJOWANIE_DESKRYPTOROW
@@ -97,19 +92,35 @@ START:
 	;wywolowanie z MAKRA.TXT PM_TASKS
   PM_TASKS TSS_0,TSS_1,GDT_TSS_0,GDT_TSS_1
 	
+	
 	;zadanie 1 ze stosem 128
+	MOV WORD PTR TSS_0+4CH, 16              ;segment programu zadania CS  (SEGMENT PROGRAMU)
+	MOV WORD PTR TSS_0+20H, OFFSET ZADANIE_1;adres powrotu IP             (SEGMENT 
+	MOV WORD PTR TSS_0+50H, 24              ; SS                          (SEGMENT STOSU)
+	MOV WORD PTR TSS_0+38H, 128             ; StackPointer                (SEGMENT wielkosc stosu)
+	MOV WORD PTR TSS_0+54H, 8               ; ogólny segment danych DS    (SEGMENT DANYCH)
+	MOV WORD PTR TSS_0+48H, 32              ; pamiec ekranu ES            (SEGMENT EKRANU)
+	
+	STI		                  ;ustawienie znacznika zestawienia na przerwanie
+	PUSHFD		              ;przeslanie znacznikow na szczyt stosu, przepisanie rejestru eflags do eax
+	POP EAX
+	
+	MOV DWORD PTR TSS_0+24H, EAX ;eeflags 
+	;28h - 47h - adresy wszystkich rejestrow roboczych 
+	
+	;zadanie 2 ze stosem 128
 	MOV WORD PTR TSS_1+4CH, 16              ;segment programu zadania CS  (SEGMENT PROGRAMU)
-	MOV WORD PTR TSS_1+20H, OFFSET ZADANIE_1;adres powrotu IP             (SEGMENT 
+	MOV WORD PTR TSS_1+20H, OFFSET ZADANIE_2;adres powrotu IP             (SEGMENT 
 	MOV WORD PTR TSS_1+50H, 24              ; SS                          (SEGMENT STOSU)
 	MOV WORD PTR TSS_1+38H, 128             ; StackPointer                (SEGMENT wielkosc stosu)
 	MOV WORD PTR TSS_1+54H, 8               ; ogólny segment danych DS    (SEGMENT DANYCH)
 	MOV WORD PTR TSS_1+48H, 32              ; pamiec ekranu ES            (SEGMENT EKRANU)
-	MOV WORD PTR TSS_1+58H, 32              ; pamiec rozszerzona
-	MOV WORD PTR TSS_1+5CH, 32              ; pamiec rozszerzona 
+	
+	MOV DWORD PTR TSS_1+24H, EAX
 	
 	CLI
 	INICJACJA_IDTR
-	KONTROLER_PRZERWAN_PM 0FEH  ;1111 1110
+	KONTROLER_PRZERWAN_PM 0FCH  ;1111 1100
 	TRYB_CHRONIONY
 	
 	MOV AX,32
@@ -126,7 +137,7 @@ START:
 	STI				        ;Uaktywnienie przerwañ w trybie chronionym
 	
 	OPOZNIENIE 10000
-OPOZNIENIE 10000
+  OPOZNIENIE 10000
 	OPOZNIENIE 10000
 	CLI
 	
@@ -138,9 +149,9 @@ OPOZNIENIE 10000
 ZADANIE_1	PROC
 	MOV BX,POZYCJA_1	
 	MOV AH,KOLOR	
-	MOV AL,21H
+	MOV AL,'#'
 	MOV ES:[BX],AX
-	ADD BX,2
+	ADD BX,1
 	MOV POZYCJA_1,BX
 	MOV AL,20H		;Sygna³ koñca obs³ugi przerwania
 	OUT 20H,AL	
@@ -151,14 +162,14 @@ ZADANIE_1	ENDP
 ZADANIE_2	PROC
 	MOV BX,POZYCJA_2	
 	MOV AH,KOLOR	
-	MOV AL,21H
+	MOV AL,'@'
 	MOV ES:[BX],AX
-	ADD BX,2
-	MOV POZYCJA_1,BX
+	ADD BX,4
+	MOV POZYCJA_2,BX
 	MOV AL,20H		;Sygna³ koñca obs³ugi przerwania
 	OUT 20H,AL	
 	IRETD
-	JMP ZADANIE_1		;Skok do pocz¹tku procedury wykonywanej w zadaniu
+	JMP ZADANIE_2		;Skok do pocz¹tku procedury wykonywanej w zadaniu
 ZADANIE_2	ENDP 
 
 
